0da2aede9e02dec2abbc9fa2230d87041d57b720
Nicky Tierenteyn <nicky.tierenteyn@vaimo.com>
2025-05-23 13:50:26 +0200

    restaurant

--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { createRestaurant, getAllRestaurants, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
 const menuItemRoutes = require('./menuItemRoutes');
 const { createRestaurant, getAllRestaurants, getRestaurantById, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
 
