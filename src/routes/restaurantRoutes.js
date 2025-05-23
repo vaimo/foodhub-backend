d581abee7c9c23713c562d4ac16257217e05946b
randi.fernandez-moran <randi.fernandez-moran@vaimo.com>
2025-05-23 09:37:35 +0200

    Add getRestaurantById function and update routes to include it

--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createRestaurant, getAllRestaurants, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
+const { createRestaurant, getAllRestaurants, getRestaurantById, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
 
 router.post('/', createRestaurant);
 
 router.get('/', getAllRestaurants);
+
+router.get('/:id', getRestaurantById);
 
 router.put('/:id', updateRestaurant);
 
